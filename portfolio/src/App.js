189aeb17dbd2b5ff9b7e1de23df3950f0ba36a87
Dyana Monroy <dyana.monroy17@live.com>
2021-05-29 14:39:17 -0400

    add footer and styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './styles/styles.css'
+import Header from './components/Header';
+import About from './pages/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Contact from './pages/Contact';
+import Footer from './components/footer';
+import Work from './pages/Work';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="container ">
+        <Router>
+        {/*This is where it can be share with all paths, the same content in each page. */}
+            <Footer></Footer>
+          <Switch>
+          <Header></Header>
+            {/**the exact is for the path so, to match only the path that has the empty slash. so don't need to change the order of the paths
+             * and each page will be unquie path.
+             */}
+            <Route path='/' exact>
+           {/*} <Resources></Resources>*/}
+            </Route>
+            <Route path='/resources/new'>
+            {/* <ResourceNew></ResourceNew>*/}
+            </Route>
+            {/*:id is to show in all path ids the same info. Also it will help resourcedetails params to identify which id we are refering to. */}
+            <Route path='/resources/:id'>
+            {/*  <ResourceDetails></ResourceDetails> */}
+            </Route>
+            <Route path='/work'>
+            <Work></Work>
+            </Route>
+
+            <Route path='/about'>
+            <About></About>
+            </Route>
+            <Route path='/contact'>
+            <Contact></Contact>
+            </Route>
+          </Switch>
+        </Router>
+
+       </div>
+
   );
 }
 
---
+++ portfolio/src/components/footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const footer = () => {
+
+  return(
+
+   <div className= "bg">
+     <div className="info">
+       <p> © Dyana Monroy 2021 | Web Developer | Web Designer | UX Designer</p>
+     </div>
+     <div className="footerlinks">
+
+    <a href="https://www.linkedin.com/in/dyana-monroy-660527156/"><i class="fab fa-linkedin-in"></i></a>
+    <a href="https://www.behance.net/dyanamonroy"><i class="fab fa-behance"></i></a>
+    <a href="https://github.com/DM54"><i class="fab fa-github-alt"></i></a>
+    <a href="mailto: dyana.monroy17@live.com"><i class="fas fa-at"></i></a>
+   </div>
+   </div>
+
+
+
+  )
+}
+
+export default footer;
