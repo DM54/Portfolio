d0ae43606bf04285761344a45859a24e4deb33aa
Dyana Monroy <dyana.monroy17@live.com>
2021-05-30 19:11:06 -0400

    edited about page

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Header from './Header';
 import mepic from '../images/me.jpg';
+import Image from 'react-bootstrap/Image'
 const About = () => {
   return (
     <div class="container">
        <Header></Header>
 
+        <br/>
+        <br/>
+        <br/>
        <div class="row">
          <div class="column">
-            <img src={ mepic} alt="me"></img>
+            <div class="stylepics">
+            <Image src={ mepic} alt="me"  roundedCircle  ></Image>
+            </div>
          </div>
          <div class="column">
+           <h1 className="name">Dyana Monroy</h1>
+           <p className="pcolorabout">
+            Dyana is a developer / designer, passionate about
+            creating applications and designs.
+           </p>
+           <p className="pcolorabout">
+
+           </p>
 
          </div>
        </div>
