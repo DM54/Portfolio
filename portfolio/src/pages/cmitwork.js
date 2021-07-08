e2a6a4be76fe4b7f3e575fd0776caf5e92b10816
Dyana Monroy <dyana.monroy17@live.com>
2021-07-08 16:44:52 -0400

    added more info for work

--- portfolio/src/pages/cmitwork.js
+++ portfolio/src/pages/cmitwork.js
@@ -60,7 +60,7 @@ const CmitWork = () =>{
       <ul className="pcolorabout psize">
       <li>I created a landing page to help small and medium size businesses prepare to return to office as a hybrid working model and
         created a sign up form from Constant Contact.</li>
-      <li>Whenever someone signs up to the mailing list they will receive an email of a PDF checklist document (to help small &
+      <li>Whenever someone signs up to the mailing list they will receive an email of a PDF checklist document (to help small and
       medium size businesses prepare to return to office as a hybrid working model).</li>
       <li>Launch it to social media platforms and advertise it in LinkedIn.</li>
       <li><a href="https://cmitsolutions.com/tribeca/hybrid-working-model/" target="_blank" rel="noreferrer">Link</a></li>
@@ -80,6 +80,89 @@ const CmitWork = () =>{
       <li>Made email campaign to launch it to social media platforms and advertise it in LinkedIn. </li>
       <li><a href="https://cmitsolutions.com/tribeca/digital-service/" target="_blank" rel="noreferrer">Link</a></li>
       </ul>
+
+      <br/>
+
+      <h5>Cyber Security page | 03 2020 </h5>
+      <p className="pcolorabout psize " >
+      Currently in DEMO <br/>
+      Role: Design <br/>
+      Skills/Software: Photoshop, WordPress - HTML5/CSS3, JavaScript, Video editing
+
+      </p>
+      <ul className="pcolorabout psize">
+      <li>Created a new cybersecurity page to improve the original web page with better images, videos </li>
+      <li>To educate the visitors/users why cybersecurity is important with infographics</li>
+      <li><a href="https://cmitsolutions.com/tribeca/cyber-security-services/" target="_blank" rel="noreferrer">Link</a></li>
+      </ul>
+
+      <br/>
+
+      <h5>Managed IT page | 09 2020 </h5>
+      <p className="pcolorabout psize " >
+      Currently in DEMO <br/>
+      Role: Design <br/>
+      Skills/Software: Photoshop, WordPress - HTML5/CSS3, JavaScript, Video editing
+
+      </p>
+      <ul className="pcolorabout psize">
+      <li> Created a new Managed IT Service page to improve the original web page with better images, videos</li>
+      <li> To inform visitors/users what is Managed IT and what it means Managed IT Providers and what they can do for the users
+          and their business</li>
+      <li><a href="https://cmitsolutions.com/tribeca/managed-it-services/" target="_blank" rel="noreferrer">Link</a></li>
+      </ul>
+
+      <br/>
+      <h3 className="green">Email Campaigns</h3>
+      <hr/>
+
+
+      <br/>
+
+      <h5>Hybrid Working model | 03 2021 </h5>
+      <p className="pcolorabout psize " >
+      Role: Design, Email Marketing, Social Media Marketing <br/>
+      Skills/Software: Constant Contact Email Automation, Photoshop
+      </p>
+      <ul className="pcolorabout psize">
+      <li><a href="https://web-extract.constantcontact.com/v1/social_annotation_v2?permalink_uri=https%3A%2F%2Fmyemail.constantcontact.com%2FWhat-is-your-Hybrid-Working-Plan--Let-s-talk-about-your-hybrid-working-plan-.html%3Fsoid%3D1134320327524%26aid%3D1SW-7i3qI6A&image_url=https%3A%2F%2Fmlsvc01-prod.s3.amazonaws.com%2F46b92ca1801%2Fc53a2a84-6f9d-4c56-8ebb-3a7914442825.png%3Fver%3D1619116355000" target="_blank" rel="noreferrer">
+      Link to view the email campaign</a></li>
+      <li> Created an email campaign for Hybrid Working model landing page and PDF Checklist Document </li>
+      <li>Distributed to existing clients and new clients and launch it to social media platforms</li>
+      </ul>
+
+      <br/>
+
+      <h5>Digital Service Offer | 10 2020 </h5>
+      <p className="pcolorabout psize " >
+      Role: Design, Email Marketing, Social Media Marketing <br/>
+      Skills/Software: Constant Contact Email Automation, Photoshop
+      </p>
+      <ul className="pcolorabout psize">
+      <li><a href="https://web-extract.constantcontact.com/v1/social_annotation_v2?permalink_uri=https%3A%2F%2Fmyemail.constantcontact.com%2FNeed-help-with-SEO-or-Web-Design-for-your-business---.html%3Fsoid%3D1134320327524%26aid%3D9G3wqG45W2k&image_url=https%3A%2F%2Ffiles.constantcontact.com%2F46b92ca1801%2F8ddeed0e-504b-49f0-a6ef-5e8ea706da70.png" target="_blank" rel="noreferrer">
+      Link to view the email campaign</a></li>
+      <li>  Created an email campaign offering Digital Service and let the existing clients and new clients know that CMIT Tribeca can
+      also offer digital marketing service.  </li>
+      <li> launch it to social media platforms </li>
+      </ul>
+
+      <br/>
+
+      <h5>Free IT Service Offer | 06 2020</h5>
+      <p className="pcolorabout psize " >
+      Role: Design, Email Marketing, Social Media Marketing <br/>
+      Skills/Software: Constant Contact Email Automation, Photoshop
+      </p>
+      <ul className="pcolorabout psize">
+      <li><a href="https://web-extract.constantcontact.com/v1/social_annotation_v2?permalink_uri=https%3A%2F%2Fmyemail.constantcontact.com%2FFree-IT-Services---Customer--Empower-Your-Business--.html%3Fsoid%3D1134320327524%26aid%3Dg_213yQLhcQ&image_url=https%3A%2F%2Fmlsvc01-prod.s3.amazonaws.com%2F46b92ca1801%2F336a4516-ab86-468d-b9e1-812d561cba35.jpg%3Fver%3D1591029505000" target="_blank" rel="noreferrer">
+      Link to view the email campaign</a></li>
+      <li> Created an email campaign offering free IT Service for at least 3 months and if the requirements meets to receive the free
+       service from CMIT Tribeca  </li>
+      <li> Launch it to social media platforms and advertise it in LinkedIn</li>
+      </ul>
+
+
+
 
 
     </div>
