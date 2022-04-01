869f78496f007253d3705af650286b7842dee55b
Dyana Monroy <dyana.monroy17@live.com>
2022-04-01 13:13:34 -0400

    updated on work process page

--- portfolio/src/pages/workprogesspage.js
+++ portfolio/src/pages/workprogesspage.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Header from './Header';
 import bigpic from '../images/uxgoogleproj/bigpicture.jpg';
 import closeup from '../images/uxgoogleproj/closeup.jpg';
-
-
+import persona1 from '../images/uxgoogleproj/persona1.PNG';
+import persona2 from '../images/uxgoogleproj/persona2.png';
+import userjourney from '../images/uxgoogleproj/personauserjourneymap.PNG';
+import stickynotes from '../images/uxgoogleproj/stickynotes.jpg';
 const uxgoogleproject = () => {
   return (
     <div class="container">
@@ -55,6 +57,10 @@ const uxgoogleproject = () => {
           storyboard - Big Picture
         </p>
         <img src={bigpic} alt="bigpicture" style={{width: "50%"}}></img>
+        <p style={{width: "50%", float: "right"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit a
+        nim id est laborum.</p>
       </div>
       <br/>
       <div className="flex">
@@ -62,6 +68,47 @@ const uxgoogleproject = () => {
           storyboard - Close-up
         </p>
         <img src={closeup} alt="closeup" style={{width: "50%"}}></img>
+
+        <p style={{width: "50%", float: "right"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit a
+        nim id est laborum.</p>
+      </div>
+
+      <div>
+        <br/>
+      <h4>Personas</h4>
+      <img src={persona1} alt="persona1" style={{width: "50%"}}></img>
+      <img src={persona2} alt="persona2" style={{width: "50%"}}></img>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit a
+        nim id est laborum.</p>
+
+      </div>
+
+      <div>
+        <br/>
+        <h4>User Journey</h4>
+        <img src={userjourney} alt="personauserjourney" style={{width: "50%"}}></img>
+        <p style={{width: "50%", float: "right"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit a
+        nim id est laborum.</p>
+        <img src={stickynotes} alt="stickynotes" style={{width: "40%", margin: "10px 0px 0px 50px"}}></img>
+
+      </div>
+
+      <div>
+        <br/>
+        <h4>Empathy Map</h4>
+        <img src={userjourney} alt="personauserjourney" style={{width: "50%"}}></img>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit a
+        nim id est laborum.</p>
+
+
       </div>
 
    </div>
